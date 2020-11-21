5b4285d510f7d473eb9c6cee839fd16a21ebdaa0
shubham mishra <sm091274@gmail.com>
2020-11-21 22:06:11 +0530

    This is first custom list view on image and string created by me

--- app/src/main/java/com/example/listviewcustom/MyAdapter.java
+++ app/src/main/java/com/example/listviewcustom/MyAdapter.java
@@ -1,10 +1,36 @@
 package com.example.listviewcustom;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-public class MyAdapter {
+import java.util.ArrayList;
 
+public class MyAdapter extends ArrayAdapter<Word>{
+    public MyAdapter(Activity context, ArrayList<Word>word) {
+        super(context, 0,word);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listitemview =convertView;
+        if(listitemview==null){
+            listitemview= LayoutInflater.from(getContext()).inflate(R.layout.one_view,parent,false);
+        }
+        Word currentworld=getItem(position);
+        TextView tv=listitemview.findViewById(R.id.mytext);
+        ImageView iv=listitemview.findViewById(R.id.myimage);
+        tv.setText(currentworld.getName());
+        iv.setImageResource(currentworld.getResourceid());
+        return listitemview;
+    }
 }
