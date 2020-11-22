de2077f775cfae1ef8dae1f09cd7c687193f1a65
shubham mishra <sm091274@gmail.com>
2020-11-22 20:52:32 +0530

    This is first custom list view on image and string created by me

--- app/src/main/java/com/example/listviewcustom/MainActivity.java
+++ app/src/main/java/com/example/listviewcustom/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.listviewcustom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -28,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter myAdapter = new MyAdapter(this,obj);
         ListView listView = findViewById(R.id.list_view);
         listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,String.valueOf(position) , Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("position",String.valueOf(position));
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/listviewcustom/MainActivity2.java
+++ app/src/main/java/com/example/listviewcustom/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.listviewcustom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView tv= findViewById(R.id.test);
+        String position=getIntent().getStringExtra("position");
+        tv.setText(String.valueOf(position));
     }
 }
